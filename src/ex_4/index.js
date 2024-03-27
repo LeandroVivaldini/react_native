60897d11e9ad9c6ac62853bef93293b3e0e20391
Leandro Vivaldini <LeandroVivaldini@users.noreply.github.com>
2024-03-26 22:47:12 -0300

    Salvo dia 26/03

--- src/ex_4/index.js
+++ src/ex_4/index.js
@@ -1,11 +1,36 @@
-import {View,Text} from 'react-native';
+import {View,Text, TextInput, Pressable} from 'react-native';
 
 import styles from './styles';
+import { useState } from 'react';
 
 export default function Exemplo4 () {
+   
+    const [mensagem, setMensagem] = useState('Sua mensagem aqui:');
+    const [mens2, setMens2] =useState('Outra mensagem...');
+    const [mensTela, setMensTela] = useState('');
     return(
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 4</Text>
+            
+            <text style={styles.txtSaida}>{mensagem}</text> 
+        
+            <TextInput 
+            style={styles.txtEntrada}
+            placeholder='Digite o texto aqui...'
+            onChangeText={(txt) => setMensagem(txt)}
+            />
+
+            <Text style={styles.txtSaida}>{mensTela}</Text>
+            <TextInput
+             style={styles.txtEntrada}
+            //  placeholder='Outro texto...'
+             onChangeText={(entrada) => setMens2(entrada)}
+             />
+            <Pressable onPress={() => setMensTela(mens2)}>
+            <Text>Exibir mensagem</Text>
+            </Pressable>
         </View>
+       
+
     );
 }
